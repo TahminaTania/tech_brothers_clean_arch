93737a37bfd121b6d0653be40bd31740b9fc4a81
TahminaTania <tahminatania1508@gmal.com>
2023-07-20 17:08:22 +0600

    got token but shared preferences isn't working yet

--- lib/repository/auth_repository.dart
+++ lib/repository/auth_repository.dart
@@ -3,12 +3,13 @@ import 'package:learn_clean_achitecture/data/network/network_api_services.dart';
 import 'package:learn_clean_achitecture/res/app_urls.dart';
 
 class AuthRepository {
-  BaseApiServices _baseapiservices = NetworkApiServices();
+  BaseApiServices _apiservices = NetworkApiServices();
 
   Future<dynamic> loginapi(dynamic data) async {
     try {
-      dynamic response = await _baseapiservices.getPostApiResponses(
-          AppUrl.loginEndPoint, data);
+      dynamic response =
+          await _apiservices.getPostApiResponses(AppUrl.loginEndPoint, data);
+      return response;
     } catch (e) {
       throw (e);
     }
@@ -16,8 +17,9 @@ class AuthRepository {
 
   Future<dynamic> signupapi(dynamic data) async {
     try {
-      dynamic response = await _baseapiservices.getPostApiResponses(
+      dynamic response = await _apiservices.getPostApiResponses(
           AppUrl.registerApiEndPoint, data);
+      return response;
     } catch (e) {
       throw (e);
     }
